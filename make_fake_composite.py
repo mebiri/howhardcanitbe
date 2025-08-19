10b508d246f3e5eb770d7e059de932dc696d87d6
mebiri <marcebiri@gmail.com>
2025-08-19 15:43:28 -0400

    updates

--- make_fake_composite.py
+++ make_fake_composite.py
@@ -29,17 +29,21 @@ parser.add_argument("--iteration-number",default=0,type=int)
 parser.add_argument("--lnL-fixed-offset",default=100,type=float,help="A large constant offset, comparable to what we expect for a modest detection, so that the tendency of the GP to return to being near 0 does not contaminate the posterior for our gaussian tests! Equivalent to setting a minimum SNR")
 #parser.add_argument("--verbose",action='store_true') #unused 
 parser.add_argument("--base-data-file",default='initgrid3.txt',help="filename/path of synthetic data file in #m1 m2 format.")
-parser.add_argument("--single",default=True,help="For testing; will only make composite file for first line in synthetic data file.") #TODO: make False by default
-parser.add_argument("--plot-composite",default=False,help="Scatterplot of generated points, using lnL as color scale.") 
+parser.add_argument("--single",action='store_true',help="For testing; will only make composite file for first line in synthetic data file.")
+parser.add_argument("--plot-composite",action='store_true',help="Scatterplot of generated points, using lnL as color scale.") 
 opts = parser.parse_args()
 
 print(opts)
 n_grid = opts.n_grid
 n_grid_post = opts.n_grid_post
 offset = opts.lnL_fixed_offset
+run_single = opts.single
+save_plot = opts.plot_composite
+gen_plot = False
 
 ## Retrieve test parameters, from current working directory
 #dat =np.loadtxt("x0.dat")[0]   # these are lines with different gaussian parameters in each line, we just need ONE of them
+
 n_dim = 2#len(dat) - hardcoding 2 dimensions for now (overwritten later)
 my_dim_list = ['m1', 'm2', 's1x', 's1y', 's1z', 's2x', 's2y', 's2z']
 my_dim_ranges = [[1.5,2], [1,1.49], [-1,1], [-1,1], [-1,1], [-1,1], [-1,1], [-1,1]] #old m1,m2 ranges: [15,25], [8,12]
@@ -50,6 +54,7 @@ m1_err, m2_err = stats.norm.rvs(scale=sigma_mass, size=2)
 print("mass errors:",m1_err,m2_err)
 
 
+#unused at present; seems unnecessary
 def get_dat():
     #dat =np.loadtxt("x0.dat")[0]   # these are lines with different gaussian parameters in each line, we just need ONE of them
     filename = opts.base_data_file
@@ -75,7 +80,7 @@ def generate_obs_grid(m1, m2, rv, idx):
     #For each mass (dim), draw an in-range random # for each line of the grid:
     for i in np.arange(n_dim):
         #This draws column by column in the grid (many lines, few columns):
-        data_grid[:n_grid,i+indx_offset] = np.array(np.random.uniform(low=my_dim_ranges[i][0], high=my_dim_ranges[i][1],size=n_grid))
+        data_grid[:n_grid, i+indx_offset] = np.array(np.random.uniform(low=my_dim_ranges[i][0], high=my_dim_ranges[i][1], size=n_grid))
     
     #(Bonus lines) Part with random draws from the posterior - first make random draws from all 
     for z in np.arange(n_grid_post):
@@ -98,8 +103,8 @@ def generate_obs_grid(m1, m2, rv, idx):
     #Default filename format: 'grid-random-0-#.txt'
     np.savetxt("grid-random-"+str(opts.iteration_number)+"-"+str(idx)+".txt",data_grid,header=prefix + (' '.join(my_dim_list)) + ' lnL lnL_err -1')
     
-    if opts.plot_composite:
-        plot_composite(data_grid)
+    if save_plot:# or gen_plot:
+        plot_composite(data_grid, idx)
         
 # =============================================================================
 # rv = stats.multivariate_normal(mean=dat, cov=sigma_mass**2 * np.diag(np.ones(n_dim)) ) # naive unit covariance
@@ -128,28 +133,41 @@ def generate_obs_grid(m1, m2, rv, idx):
 # =============================================================================
     
 
-def plot_composite(grid):
+def plot_composite(grid, idx):
     import matplotlib.pyplot as plt
     
     #Scatterplot:
     fig1 = plt.figure(figsize=(8,5),dpi=250) 
     ax = fig1.add_subplot(111)
-    ax.scatter(grid[:,1],grid[:,2],c=grid[:,9])
+    ax.scatter(grid[:,indx_offset],grid[:,indx_offset+1],c=grid[:,indx_offset+n_dim+6])
     ax.set_xlabel("x mass", size="11")
     ax.set_ylabel("y mass", size="11")
     ax.tick_params(axis='both', which='major', labelsize=10) 
     fig1.tight_layout()
-    plt.show(block=False)
+# =============================================================================
+#     if gen_plot:
+#         plt.show(block=False)
+#     if save_plot:
+#         plotname = "grid-random-"+str(opts.iteration_number)+"-"+str(idx)+".png"
+#         plt.savefig(plotname,format = 'png')
+#         print("Scatterplot saved as "+plotname)
+# =============================================================================
     
 
 if __name__ == '__main__':
     #Tasty defaults:
     #TODO remove these
-    n_grid = 1000
-    n_grid_post = 0
-    
+    if n_grid == 200: #assume running locally 
+        n_grid = 1000
+        n_grid_post = 0
+        run_single = True
+        save_plot = True
+        gen_plot = False #TODO set to False
+        print("Local settings: n_grid=",n_grid,"n_grid_post=",n_grid_post,"single=",run_single,"plot: save",save_plot,"gen",gen_plot)
+        
     #exact ("true") obs file (should contain masses or mc/eta?):
-    x = get_dat()
+    #x = get_dat() #seems pretty unnecessary
+    x = np.genfromtxt(opts.base_data_file,dtype='str')
     n_dim = len(x[0])
     
     for idx in range(len(x)):
@@ -160,7 +178,7 @@ if __name__ == '__main__':
         #rv = stats.multivariate_normal(mean=dat, cov=sigma_mass**2 * np.diag(np.ones(n_dim))) #naive unit covariance
         rv = stats.multivariate_normal(mean=[mu1,mu2], cov=(sigma_mass**2)*np.diag(np.ones(n_dim)))
         generate_obs_grid(mu1, mu2, rv, idx)
-        if opts.single:
+        if run_single:
             break
     print("Done.")
     
