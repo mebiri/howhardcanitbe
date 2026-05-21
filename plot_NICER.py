28e9ff72f294ee8139b7e42dde995eadbb3da328
mebiri <marcebiri@gmail.com>
2026-05-21 01:42:04 -0400

    added opts, rewrote for flexibility

--- plot_NICER.py
+++ plot_NICER.py
@@ -11,9 +11,18 @@ from scipy.stats import multivariate_normal
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--obs-file', action='append', help="REQUIRED: Filenames (NOT PATHS) for observations used for likelihood calculation and plots generated here.")#Supported: j0740 j1731 j0030 j0437")
-parser.add_argument('--mode',type=str,help="Supported opts: data-only, data-field")
+parser.add_argument('--obs-file', action='append', help="REQUIRED: Filenames/paths for observations to use.")
+parser.add_argument('--mode',type=str,help="REQUIRED: Supported opts: data-only, data-field")
 parser.add_argument('--eos-max-m',type=float,help="Max mass to truncate mass range (pretend EOS), if wanted")
+parser.add_argument('--use-lnL',action='store_true',help="If present, uses log likelihoods (recommended)")
+parser.add_argument('--lnL-cut',type=float,default=1e-6,help="Value to which smaller likelihoods/lnLs are adjusted to.")
+parser.add_argument('--data-on-top',action='store_true',help="Whether to render obs data above or beneath mass field")
+parser.add_argument('--npts',type=int,default=1000,help="Number of points to use in field (default 1000)")
+parser.add_argument('--field-M-min',type=float,default=0.4)
+parser.add_argument('--field-M-max',type=float,default=2.1)
+parser.add_argument('--field-R-min',type=float,default=8)
+parser.add_argument('--field-R-max',type=float,default=18)
+parser.add_argument('--scale-lnL',type=int,default=1,help="Scale factor to apply to likelihood calcs (applied before log)")
 
 opts = parser.parse_args()
 
@@ -22,10 +31,12 @@ def just_obs():
     ax = fig1.add_subplot(111)
     
     for i in opts.obs_file:
-        if i[0] == 'J':
+        print("Retrieving data from file: "+i)
+        starfile = i.split("/")[-1]
+        if starfile[0] == 'J':
             starname = i[:5]
         else:
-            starname = i.split(".")[0]
+            starname = starfile.split(".")[0]
         dat = np.genfromtxt(i)
         print("len this file:",len(dat))
         R = dat[:1000,0]
@@ -33,17 +44,17 @@ def just_obs():
     
         ax.scatter(R,M,marker=".",label=starname)
         
-    #ax.set_xlim(left=1.0,right=2.0)
-    ax.set_ylim(bottom=1.0,top=2.0)
-    ax.set_xlabel("radius", size="11")
-    ax.set_ylabel("mass", size="11")
+    #ax.set_xlim(left=8.0,right=24.0)
+    #ax.set_ylim(bottom=0.3,top=2.5)
+    ax.set_xlabel("Radius (km)", size="11")
+    ax.set_ylabel('Mass (M/M$_\odot$)', size="11")
     ax.tick_params(axis='both', which='major', labelsize=10) 
     ax.grid(True)
     ax.legend(fontsize='9', loc='lower right')
     fig1.tight_layout()
     plt.show()
     
-    plt.savefig("nicer_data.png")
+    plt.savefig("MR_NICER_data.png")
     print("Saved.") 
 
 
@@ -53,10 +64,11 @@ def gaussian_distribution(data):
     '''
     mn = np.mean(data,axis=0)
     cov = np.cov(data.T)
+    print("mean of this dataset: (R,M) =",mn)
+    print("cov:\n",cov)
     a, b = 0,1
     
     x, y = np.mgrid[min(data[:,a]):max(data[:,a]):.01, min(data[:,b]):max(data[:,b]):.01]
-    #pos = np.dstack((x, y))
     #2D normal dist, axes mass vs. radius
     rv = multivariate_normal([mn[a], mn[b]], [[cov[a,a], cov[a,b]], [cov[a,b], cov[b,b]]])
     
@@ -65,29 +77,28 @@ def gaussian_distribution(data):
 
 def obs_and_MR():
     import matplotlib
-    import matplotlib.pyplot as plt
     
     #mass grid
-    dM = 0.001
-    Mmax = 2.1
-    Mmin = 0.4
+    #dM = 0.001
+    Mmax = opts.field_M_max
+    Mmin = opts.field_M_min
     #M = np.arange(Mmin,Mmax,dM)  # range of masses
-    M = np.random.uniform(Mmin,Mmax,1000)
+    M = np.random.uniform(Mmin,Mmax,opts.npts)
     if opts.eos_max_m is not None:
         M = M[np.where(M <= opts.eos_max_m)]
     #radius grid
     #R = reprimand_object.circ_radius_from_grav_mass(M)*reprimand_object.units_to_SI.length/1e3
-    Rmax = 25
-    Rmin = 8
-    dR = (Rmax-Rmin)/len(M)
+    Rmax = opts.field_R_max
+    Rmin = opts.field_R_min
+    #dR = (Rmax-Rmin)/len(M)
     #R = np.arange(8,25,dR)
     R = np.random.uniform(Rmin,Rmax,len(M))
     print("len M:",len(M),"; len R:",len(R))
-    assert len(M) == len(R)
-    scale_factor = (Mmax-Mmin)/(max(M)-min(M))
+    scale_factor = opts.scale_lnL*(Mmax-Mmin)/(max(M)-min(M))
+    print("scale factor:",scale_factor)
     
     like_grid = np.zeros((len(M),len(opts.obs_file)))
-    print("like grid size:",like_grid.shape)
+    print("likelihood grid size:",like_grid.shape)
         
     #Create 2D Gaussians for each provided NICER data set:
     dat_rv = [] #formerly external_ns_MR_rv
@@ -98,12 +109,13 @@ def obs_and_MR():
     obs_num = 0
     for obs in opts.obs_file:
         print("Retrieving data from file: "+obs)
-        if obs[0] == 'J':
-            stars.append(obs[:5])
+        starfile = obs.split("/")[-1]
+        if starfile[0] == 'J':
+            stars.append(starfile[:5])
         else:
-            stars.append(obs.split(".")[0])
+            stars.append(starfile.split(".")[0])
         
-        dat_here = np.genfromtxt(obs)
+        dat_here = np.genfromtxt(obs)[:,:2] #only get 1st 2 cols (R,M)
         mn, cov, rv = gaussian_distribution(dat_here)
         dat_rv.append(rv)
         dat_mn.append(mn)
@@ -111,14 +123,35 @@ def obs_and_MR():
         dat_list.append(dat_here)    
     
 
-        likelihood_local_list = obs.pdf(np.c_[R,M])*scale_factor
-        print("likelihood res:",np.sum(likelihood_local_list))
-        #likelihood_dict.append(np.sum(likelihood_local_list))
-        like_grid[:,obs_num] = likelihood_local_list
-      
-    likelihood_array = np.ones(len(M))
-    for i in np.arange(len(M)):
-        for res in like_grid[i]: likelihood_array[i] *= res     
+        likelihood_local_list = rv.pdf(np.c_[R,M])*scale_factor
+        print("total likelihood:",np.sum(likelihood_local_list))
+        if opts.use_lnL:
+            likelihood_local_list = np.log(likelihood_local_list)
+        if opts.lnL_cut is not None:
+            likelihood_truncate_list = np.where(likelihood_local_list > opts.lnL_cut, likelihood_local_list, opts.lnL_cut)
+        else:
+            likelihood_truncate_list = likelihood_local_list
+        like_grid[:,obs_num] = likelihood_truncate_list
+        obs_num += 1
+    
+    print("Test, first line eval:")
+    if opts.use_lnL:
+        #likelihood_array = np.zeros(len(M))  
+        res1 = 0.
+        for res in like_grid[0]: res1 += res
+        likelihood_array = np.sum(like_grid,axis=1)
+    else:
+        #likelihood_array = np.ones(len(M))
+        res1 = 1.
+        for res in like_grid[0]: res1 *= res
+        likelihood_array = np.prod(like_grid,axis=1)
+    print(like_grid[0]," = ",res1) 
+    print("Sample of likelihood_array:\n",likelihood_array[:10])
+    
+    #for j in np.arange(len(like_grid[0])):
+    #    like_arr2[:] += like_grid[:,j]   
+    #for i in np.arange(len(M)):
+    #    for res in like_grid[i]: likelihood_array[i] *= res     
     
     
     matplotlib.rcParams.update({'font.size': 12.0,  'mathtext.fontset': 'stix'})
@@ -128,24 +161,28 @@ def obs_and_MR():
     matplotlib.rcParams['ytick.labelsize'] = 15.0
     matplotlib.rcParams['axes.labelsize'] = 25.0
     matplotlib.rcParams['lines.linewidth'] = 2.0
-    plt.style.use('seaborn-v0_8-whitegrid')
+    #plt.style.use('seaborn-v0_8-whitegrid')
     #fig,(ax1,ax2) = plt.subplots(2,1)
     
     fig = plt.figure(); ax = fig.add_subplot(111)
     
-    ax.scatter(R,M,c=likelihood_array,marker=".")
+    if opts.data_on_top:
+        ax.scatter(R,M,c=likelihood_array,marker=".")
 
     from plotting import plot_data_and_gaussian
     for j in np.arange(len(dat_rv)):
+        print("Plotting data for: ",stars[j])
         plot_data_and_gaussian(dat_mn[j],dat_cov[j],dat_rv[j],dat_list[j],ax)
     
-    #likelihood_array = np.empty((0))
-    #for i in likelihood_dict: likelihood_array = np.append(likelihood_array, likelihood_dict[i])
+    if not opts.data_on_top:
+        ax.scatter(R,M,c=likelihood_array,marker=".")
     
     lmax = max(likelihood_array)
-    lmax_loc = likelihood_array.index(lmax)
-    lmin = min(likelihood_array)
+    lmax_loc = np.where(likelihood_array == lmax)[0][0] #index(lmax)
+    #lmin = min(likelihood_array)
     print("Max likelihood:",lmax,"at (R, M) = (",R[lmax_loc],M[lmax_loc],")")
+    print("Mean likelihood:",np.mean(likelihood_array))
+    print("First element in list:",likelihood_array[0])
     
     #for i in likelihood_dict:
         #MRcolor = plt.cm.gist_rainbow((likelihood_dict[i]-lmin)/(lmax-lmin))
@@ -157,7 +194,6 @@ def obs_and_MR():
     ax.set_xlabel('Radius [km]')
     ax.set_ylabel('Mass [M/M$_\odot$]')
     
-    #plt.savefig('MR_likelihood_sample_mMax.pdf',format = 'pdf')
     plt.savefig('MR_likelihood_sample_mMax_field.png',format = 'png')
     
     plt.show()
